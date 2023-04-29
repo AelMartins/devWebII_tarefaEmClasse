f0080b119be4e2e0e8cac7c89ba60271657f875a
AelMartins <oneapcwork@gmail.com>
2023-04-29 01:14:55 -0300

    created db data and update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const { pool } = require("./database/data");
 const app = express();
 app.use(express.json());
 
 app.listen(8080, () => {
     console.log("O SERVIDOR ESTÁ ATIVO NA PORTA 8080");
 })
+
+
