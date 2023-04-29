8323fa2e80de5d2a8e4c624e857f632e6904609e
AelMartins <oneapcwork@gmail.com>
2023-04-29 02:53:01 -0300

    README.md file created, added POST, PUT, DELETE to index.js file and update other files

---
+++ README.md
@@ -0,0 +1,6 @@
+<h2>Este projeto tem objetivo avaliativo acadêmico. FATEC 2023</h2>
+
+Intenção de criar conecção entre servidor e banco de dados PostegreSQL.
+
+Foi usado arquivo `.gitignore` para ignorar os módulos do Nodemon e os dados sensiveis do banco.
+* Necessário instalar `npm i express nodemon pg dotenv` apartir do terminal para o projeto.
--- index.js
+++ index.js
@@ -8,3 +8,47 @@ app.listen(8080, () => {
 })
 
 
+// POST
+app.post('/novoUsuario/:id/:name', async (req, res) =>{
+    try {
+        const client = await pool.connect();
+        const { id, name } = req.params;
+        const set = await client.query(`INSERT INTO Usuarios (id, nome) VALUES (${id}, '${name}')`);
+        const { rows } = await client.query (`SELECT * FROM Usuarios`);
+        console.table(rows);
+        res.status(201).send(`NOVO USUÁRIO INSERIDO COM SUCESSO!`);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send(`ERRO DE CONEXÃO COM SERVIDOR!`);
+    }
+});
+
+// PUT
+app.put('/atualizarUsuario/:id/:name', async (req, res) =>{
+    try {
+        const client = await pool.connect();
+        const { id, name } = req.params;
+        const up = await client.query (`UPDATE Usuarios SET nome='${name}' WHERE id = '${id}'`);
+        const { rows } = await client.query (`SELECT * FROM Usuarios`);
+        console.table(rows);
+        res.status(200).send(`USUÁRIO ATUALIZADO COM SUCESSO!`);
+    } catch (error) {
+        console.error(error);
+        return res.status(400).send(`REQUISIÇÃO DE ATUALIZAÇÃO INVALIDADA!`);
+    }
+});
+
+// DELETE
+app.delete('/deletarUsuario/:id', async (req, res) =>{
+    try {
+        const client = await pool.connect();
+        const { id } = req.params;
+        const del = await client.query(`DELETE FROM Usuarios WHERE id=${id}`);
+        const { rows } = await client.query (`SELECT * FROM Usuarios`);
+        console.table(rows);
+        res.status(200).send(`USUÁRIO DELETADO COM SUCESSO!`);
+    } catch (error) {
+        console.error(error);
+        res.status(400).send(`REQUISIÇÃO DE EXCLUSÃO INVALIDADA!`);
+    }
+});
